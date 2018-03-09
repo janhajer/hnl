66ed541fefcb5ed144d7df75f4f3d3b66b1c69f1
Jan Hajer <jan@hajer.com>
2018-03-09 15:19:36 +0100

    coding

--- main.cpp
+++ main.cpp
@@ -286,7 +286,7 @@ auto get_mass(std::string const& run, int number)
         boost::split(strings, line, [](char c) {
             return c == ' ';
         }, boost::token_compress_on);
-        if (strings.size() > 2 && strings.at(0) == std::to_string(9900012) && strings.at(2) == "#" && strings.at(2) == "mn1") return strings.at(1);
+        if (strings.size() > 2 && strings.at(0) == std::to_string(9900012) && strings.at(2) == "#" && strings.at(3) == "mn1") return strings.at(1);
     }
     return "Mass value not found"s;
 }
