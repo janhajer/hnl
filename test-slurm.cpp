53ee5875eb6af2b95ab90255376aa1b3238a3cbb
Jan Hajer <jan@hajer.com>
2020-09-02 18:39:10 +0200

    minor

--- test-slurm.cpp
+++ test-slurm.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char** argv) {
     std::vector<std::string> arguments(argv + 1, argv + argc);
-    std::ofstream file(arguments.empty() ? "testing-slurm.txt" : arguments.at(0) + ".txt");
+    std::ofstream file(arguments.empty() ? "testing-slurm.dat" : arguments.at(0) + ".dat");
     file << "Writing this to a file.\n";
     file << arguments.empty() ? "testing-slurm.txt" : arguments.at(0) + ".txt";
 }
