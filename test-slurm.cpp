9488359303b723f095fdc527a93ebe21f933b3be
Jan Hajer <jan@hajer.com>
2020-09-03 14:13:41 +0200

    fix

--- test-slurm.cpp
+++ test-slurm.cpp
@@ -7,5 +7,5 @@ int main(int argc, char** argv) {
     std::vector<std::string> arguments(argv + 1, argv + argc);
     std::ofstream file(arguments.empty() ? "testing-slurm.dat" : arguments.at(0) + ".dat");
     file << "Writing this to a file.\n";
-    file << arguments.empty() ? "testing-slurm.txt" : arguments.at(0) + ".txt";
+    file << (arguments.empty() ? "testing-slurm.txt" : arguments.at(0) + ".txt");
 }
