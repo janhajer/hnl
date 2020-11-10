32c8520b9e5722ac59639ffde18c273bb63cb338
Jan Hajer <jan@hajer.com>
2020-11-10 19:21:29 +0100

    delete

--- main-read-hepmc-simplifed.cpp
+++
@@ -1,9 +0,0 @@
-#include "read-hepmc.hh"
-
-int main(int argc, char** argv) {
-    std::vector<std::string> arguments(argv + 1, argv + argc);
-    hnl::hepmc::read_simplified(
-        arguments.size() < 1 ? "0.500000.hep" : arguments.at(0),
-        arguments.size() < 2 ? 1 : hnl::to_double(arguments.at(1))
-    );
-}
--- main-read-lhe.cpp
+++
@@ -1,6 +0,0 @@
-#include "read-lhe.hh"
-
-int main(int argc, char** argv) {
-    std::vector<std::string> arguments(argv + 1, argv + argc);
-    hnl::print(hnl::lhe::read(arguments.empty() ? "test.lhe" : arguments.front(), 1));
-}
